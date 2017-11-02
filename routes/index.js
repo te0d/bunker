07eb7d5f66a2f46fd1ddfb2bb98486e0329ab175
Tom O'Donnell <todonnel91@gmail.com>
2017-11-02 21:06:34 +0000

    Installed 'ipfs-cluster-api' to Add Basic Info to Mainpage
    Installed ipfs-cluster-api via my github repo. Used API to load
    some basic info listing pins and peers (not very useful currently).
    
    Modified error page. Any rejected promises are immediately sent to
    the error page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var ipfsClusterAPI = require('ipfs-cluster-api');
+var ipfsCluster = ipfsClusterAPI();                 // TODO:  allow non-default endpoints
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bunker' });
+  var promises = [
+    ipfsCluster.id(),
+    ipfsCluster.pin.ls(),
+    ipfsCluster.peers.ls()
+  ];
+
+  Promise.all(promises)
+    .then((info) => {
+      var id = info[0].id;
+      var pins = info[1].map((pin) => { return pin.cid });
+      var peers = info[2].map((peer) => { return peer.id});
+
+      res.render('index', {
+        title: 'Bunker',
+        id: info[0].id,
+        pins: pins,
+        peers: peers
+      });
+    }).catch((err) => {
+      res.render('error', { error: err });
+    })
 });
 
 module.exports = router;
