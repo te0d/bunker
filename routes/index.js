b3996b3c074450ce5ba1ca3ccbb5c862c97e15c8
Tom O'Donnell (te0d) <todonnel91@gmail.com>
2017-11-15 14:49:28 -0500

    Sort Pins and Remove Download for Folders
    Haven't looked into it, but download for folders has been
    problematic. Removing "Download" for now. Pins used to appear in a
    random order. Now sorted by isDir, name, cid in that order.

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,19 @@ router.get('/', function(req, res, next) {
       for (var i = 0; i < pins.length; i++) {
         pins[i].isDir = stats[i].NumLinks > 0;
       }
+      var sortedPins = pins.sort((a, b) => {
+        // Directories take priority, then name, then fallback to CID
+        var sortResult = 0;
+        var areSameType = a.isDir == b.isDir;
+        if (!areSameType) {
+          sortResult = a.isDir ? -1 : 1;
+        } else if (a.name != b.name) {
+          sortResult = a.name > b.name ? 1 : -1;
+        } else {
+          sortResult = a.cid > b.cid ? 1 : -1;  // assumes CIDs never equal
+        }
+        return sortResult;
+      });
 
       res.render('index', {
         title: 'Bunker',
